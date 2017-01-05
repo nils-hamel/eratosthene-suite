ef7189714769d192d6fed23ed77ae44c8625b0cb
nils-hamel <nils.hamel@bluewin.ch>
2017-01-05 14:26:30 +0100

    Documentation and revision of client geodesy module

--- src/eratosthene-client/src/eratosthene-client-geodesy.h
+++ src/eratosthene-client/src/eratosthene-client-geodesy.h
@@ -70,32 +70,92 @@
     header - function prototypes
  */
 
+    /*! \brief distance methods
+     *
+     *  This function computes the distance that separates the point of view
+     *  provided as parameter to the pseudo-center of the provided cell.
+     *
+     *  To compute the distance, the function considers the spatial index hold
+     *  by the cell address. It determines the parametric center of the cell and
+     *  converts it into earth-attached cartesian coordinates. The coordinates
+     *  of the point of view are also converted in cartesian coordinates.
+     *
+     *  The distance is then computed between these two points using a simple
+     *  euclidian distance. The distance, expressed in metres, is then returned
+     *  by the function.
+     *
+     *  \param er_cell Cell structure
+     *  \param er_view View structure
+     *
+     *  \return Returns the distance between the cell and the point of view
+     */
+
     le_real_t er_geodesy_distance( le_address_t const * const er_cell, er_view_t const * const er_view );
 
+    /*! \brief model methods
+     *
+     *  This function computes and returns the highest distance around the point
+     *  of view beyond that a cell is not considered by the model.
+     *
+     *  The computation is made considering only the height of the point of view
+     *  following the logic : as the height gets greater, the view limit of cell
+     *  has to be increased.
+     *
+     *  \param er_altitude Point of view height above WGS84 ellipsoid
+     *
+     *  \return Returns cell maximum distance
+     */
+
     le_real_t er_geodesy_limit( le_real_t const er_altitude );
+
+    /*! \brief model methods
+     *
+     *  This function computes, accroding to the distance between the cell and
+     *  the point of view, the ideal scale (i.e. the ideal number of digits of
+     *  is address statial index) at which a cell is relevant to display. The
+     *  two other provided values are used to clamp the result.
+     *
+     *  This function is mainly used by the model update process. As it parses
+     *  the possible cells, it checks using this function if a given cell is
+     *  relevant for display of it the enumeration of the possible cells has to
+     *  continue.
+     *
+     *  \param er_distance Distance between the cell and the point of view
+     *  \param er_scale    Server maximum number of scale (space parameter)
+     *  \param er_depth    Cell address additionnal depth (span)
+     *
+     *  \return Returns the ideal depth value
+     */
 
     le_real_t er_geodesy_depth( le_real_t const er_distance, le_size_t const er_scale, le_size_t const er_depth );
 
     /*! \brief model functions
      *
-     *  This function computes the scale factor applied to the model in function
-     *  of the distance of the point of view. The altitude, here the radial
-     *  distance to the center of earth, is considered as the only parameter
-     *  that drive the scale factor.
+     *  This function computes the scale factor applied to the model according
+     *  to the point of view height.
      *
-     *  \param er_altitude Radial position of the point of view
+     *  As the height of the point of view gets greater, the scale factor is
+     *  reduced. This allows, using simultaneously the near and far planes
+     *  modulation functions, to consider the entire earth within a single
+     *  continuous model.
      *
-     *  \return Scale factor
+     *  \param er_altitude Point of view height above WGS84 ellipsoid
+     *
+     *  \return Returns the model scale factor
      */
 
     le_real_t er_geodesy_scale( le_real_t const er_altitude );
 
     /*! \brief model functions
      *
-     *  This function computes the position of the near plane used by OpenGL
-     *  according to the radial position of the point of view.
+     *  This function computes the position of the near plane used by opengl.
      *
-     *  \param er_altitude Radial position of the point of view
+     *  The position of the near and far planes are computed according to the
+     *  point of view height that drives the model scale factor. These two
+     *  planes are adapted to the scale factor and earth size to decrease as
+     *  much as possible the precision load applied on the depth buffer.
+     *
+     *  \param er_altitude Point of view height above WGS84 ellipsoid
      *
      *  \return Near plane distance
      */
@@ -104,12 +164,16 @@
 
     /*! \brief model functions
      *
-     *  This function computes the position of the far plane used by OpenGL
-     *  according to the radial position of the point of view.
+     *  This function computes the position of the far plane used by opengl.
      *
-     *  \param er_altitude Radial position of the point of view
+     *  The position of the near and far planes are computed according to the
+     *  point of view height that drives the model scale factor. These two
+     *  planes are adapted to the scale factor and earth size to decrease as
+     *  much as possible the precision load applied on the depth buffer.
      *
-     *  \return Fat plane distance
+     *  \param er_altitude Point of view height above WGS84 ellipsoid
+     *
+     *  \return Far plane distance
      */
 
     le_real_t er_geodesy_far( le_real_t const er_altitude );
