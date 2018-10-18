3ef2bbd58318c50e789a2c0b23114701e1667697
nils-hamel <nils.hamel@bluewin.ch>
2018-10-18 14:49:15 +0200

    Discarding empty cell after server query in client

--- src/eratosthene-client/src/eratosthene-client-model.c
+++ src/eratosthene-client/src/eratosthene-client-model.c
@@ -303,16 +303,11 @@
                 /* read socket-array */
                 er_cell_set_array( er_model->md_cell + er_model->md_free, er_model->md_sock );
 
-                /* sychronise cell addres */
+                /* sychronise cell address */
                 er_parse = er_cell_set_sync( er_model->md_cell + er_model->md_free, & er_model->md_addr, er_parse );
 
                 /* process d-cell */
-                if ( er_cell_set_data( er_model->md_cell + er_model->md_free ) == 0 ) {
-
-                    /* update d-cell state */
-                    er_cell_set_flag( er_model->md_cell + er_model->md_free, ER_CELL_SYN );
-
-                } else {
+                if ( er_cell_set_data( er_model->md_cell + er_model->md_free ) != 0 ) {
 
                     /* update d-cell state */
                     er_cell_set_flag( er_model->md_cell + er_model->md_free, ER_CELL_SYN | ER_CELL_DIS );
